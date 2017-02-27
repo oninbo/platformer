b952086aea9babe1a90bab782c173c31bdaa9c4d
Elena Lebedeva <oninbo@gmail.com>
2017-02-27 14:46:14 +0300

    added game

--- main.lua
+++ main.lua
@@ -10,12 +10,5 @@ function love.load()
 end
 
 function love.draw()
-    for x=0, map.width-1 do
-        for y=0, map.height-1 do
-            local i = map.get(x,y)
-            if i ~= 0 then
-                tiles.draw(i, x, y)
-            end
-        end
-    end
+
 end
